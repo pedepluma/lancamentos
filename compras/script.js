2c0a006a77a7ea774f7ebe90cdbc026e7d997874
pedepluma <pedeplumapetrolina@gmail.com>
2024-02-02 02:41:35 -0300

    f

--- compras/script.js
+++ compras/script.js
@@ -78,37 +78,39 @@ const carrinhoCompras = () => {
 }
 
 //FORMA DE PAGAMENTO
-const FormaPagamento = () => {
-  let formasPagamento = document.getElementsByName('pagamento');
-  for (let i = 0; i < formasPagamento.length; i++) {
-    formasPagamento[i].addEventListener('change', mostrarTroco);
-  }
+// const FormaPagamento = () => {
+//   let formasPagamento = document.getElementsByName('pagamento');
+//   for (let i = 0; i < formasPagamento.length; i++) {
+//     formasPagamento[i].addEventListener('change', mostrarTroco);
+//   }
 
-  function mostrarTroco() {
-    let escolhaPagamento = document.querySelector('input[name="pagamento"]:checked').value;
-    let trocoSection = document.getElementById('trocoSection');
+//   function mostrarTroco() {
+//     let escolhaPagamento = document.querySelector('input[name="pagamento"]:checked').value;
+//     let trocoSection = document.getElementById('trocoSection');
 
-    if (escolhaPagamento === 'DINHEIRO') {
-      trocoSection.style.display = 'block';
-    } else {
-      trocoSection.style.display = 'none';
-    }
-  }
+//     if (escolhaPagamento === 'DINHEIRO') {
+//       trocoSection.style.display = 'block';
+//     } else {
+//       trocoSection.style.display = 'none';
+//     }
+//   }
 
 
+// }
+
+// let enviar = document.querySelector("#enviarZap")
+// enviar.addEventListener("click", () => {
+//   FormaPagamento()
+//   enviarZap();
+// })
+
+
+const IniciarPagina=()=>{
+  carrinhoCompras()
+ // FormaPagamento()
 }
 
-let enviar = document.querySelector("#enviarZap")
-enviar.addEventListener("click", () => {
-  FormaPagamento()
-  enviarZap();
-})
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  carrinhoCompras()
-  FormaPagamento()
-});
+document.addEventListener('DOMContentLoaded', IniciarPagina);
 
 
 
